9eb1c30b13de6680aa0595b2c61809865c501958
Koshak <kokolen77@gmail.com>
2023-02-27 07:31:56 +0300

    full

--- js/map.js
+++ js/map.js
@@ -1,4 +1,3 @@
-
 let myMap = NaN;
 let geoMarks = {};
 let names = {};
@@ -12,34 +11,33 @@ var socket = new WebSocket("ws://188.120.227.130:8765");
 
 socket.onopen = function() {
     let searchParams = new URLSearchParams(window.location.search)
-    //ids = searchParams.getAll('id[]')
-    ids = [12,245]
+    ids = searchParams.getAll('id[]')
     socket.send(JSON.stringify({'type':'subscribe', 'id':ids}))//todo:тут будут id из url
 };
 
 socket.onmessage = function(event) {
-    console.log(event.data)
+    add_marker_test(JSON.parse(event.data)['id'], JSON.parse(event.data)['coords'])
 };
 
 function init() {
-     myMap = new ymaps.Map("map", {
-            center: [55.76, 37.64],
-            zoom: 10
-        })
+    myMap = new ymaps.Map("map", {
+        center: [55.76, 37.64],
+        zoom: 10
+    })
 }
 
-function add_marker(id, coordinates){
+function add_marker(id, coords) {
     myMap.geoObjects.remove(geoMarks[id])
     geoMarks[id] = new ymaps.GeoObject({
-        geometry: {
-            type: "Point",
-            coordinates: coordinates,
-            preset: 'islands#redIcon',
+            geometry: {
+                type: "Point",
+                coordinates: coords,
+                preset: 'islands#redIcon',
+            },
+            properties: {
+                iconContent: names[id],
+            },
         },
-        properties: {
-            iconContent: names[id],
-        },
-    },
         {
             preset: 'islands#blueStretchyIcon',
         });
@@ -47,26 +45,7 @@ function add_marker(id, coordinates){
 
 }
 
-
-function add_marker_loop(id, cords, i){
-    setTimeout(function () {
-        add_marker(id, cords)
-        cords[0] += 0.002;
-        cords[1] += 0.002;
-        if ( i < 10){
-            add_marker_loop(id, cords, i+1)
-        }
-    }, 1000)
+function add_marker_test(id, coords) {
+    names[id] = id;
+    add_marker(id, coords);
 }
-
-
-
-function add_marker_test(){
-    names[1] = '1';
-    names[2] = '2';
-    cords1 = [55.75, 37.50];
-    add_marker_loop(1, cords1, 0);
-    cords2 = [53.75, 37.50];
-    add_marker_loop(2, cords2, 0);
-}
-
---
+++ js/tracker_pick.js
@@ -0,0 +1,35 @@
+function add_chechkboxes(){
+
+    $.ajax({
+        url: "http://188.120.227.130/get_active_markers",
+        success: function (data) {
+            JSON.parse(data)
+            console.log(data)
+            for (let i = 0; i < data.length; i++){
+                let add = '<div> <input type="checkbox" value="?"> <label>Kraken</label> <label> 188.188.188.0</label> </div>'
+                add = add.replace('?', data[i]['id'])
+                console.log(add);
+                $('#trackers_fields').append(add)
+            }
+
+        },
+
+    })
+
+}
+
+function redirect_to_map(){
+    let params = ''
+    $('form :input[type=checkbox]').each( function (){
+        let input = $(this);
+        if (input.is(':checked')) {
+            if (params.length === 0){
+                params+='id[]='+input.val();
+            } else{
+                params+='&id[]='+input.val();
+
+            }
+        }
+    })
+    window.location = "map.html?"+params;
+}
